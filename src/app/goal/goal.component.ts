e3603f1f10e9dc04300691f8a6b482c69481b7d3
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-12 11:18:07 +0300

    This commits adds Routing, Router-outlet and notfound components/404 pages

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
 import { Goal } from '../goal';
+import { Quote } from '../quote-class/quote';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
+
 
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css']
 })
-export class GoalComponent implements OnInit {
+export class GoalComponent implements OnInit { 
 
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2021,3,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2022,0,19)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2022,5,10)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord', new Date(2022,1,1)),
-  ];
-  
+  goals:Goal[];
+  alertService: AlertService;
+  quote!:Quote;
+
+
+
+  addNewGoal(goal: any){
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
   toggleDetails(index: any){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
@@ -26,25 +35,36 @@ export class GoalComponent implements OnInit {
       let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
       if(toDelete){
       this.goals.splice(index,1);
+      this.alertService.alertMe("The goal has been deleted")
       }
     }
   }
 
-  addNewGoal(goal: any){
-    let goalLength = this.goals.length;
-    goal.id = goalLength+1;
-    goal.completeDate = new Date(goal.completeDate)
-    this.goals.push(goal)
-
+  constructor(goalService:GoalService, alertService:AlertService,private quoteService:QuoteRequestService){
+    //private http:HttpClient) 
+    this.goals = goalService.getGoals()
+    this.alertService = alertService;
   }
-
-
-
-
-  constructor() { }
+  
 
   ngOnInit() {
+
+    this.quoteService.quoteRequest()
+    this.quote = this.quoteService.quote
+
+    // interface ApiResponse{
+    //   author:string;
+    //   quote:string;
+    // }
+
+    // this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+    //   // Succesful API request
+    //   this.quote = new Quote(data.author, data.quote)
+    // },err=>{
+      // this.quote = new Quote("Winston Churchill","Never never give up!")
+      // console.log("An error occured")
+    }
   }
 
-}
+
 
